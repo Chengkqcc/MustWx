743e19297f2c27b3488f98408e66f5a839b18b5a
DESKTOP-NPRJ350\吴大帅 <2066253363@qq.com>
2022-06-08 21:20:29 +0800

    首页与合同的搜索

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,151 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
+    // 常用服务
+    "server": [
+      {
+        "id": 0,
+        "imgsrc": "https://oss2.1dq.com/app/static/app-plus/images/add.png",
+        "text": "上传"
+      },
+      {
+        "id": 1,
+        "imgsrc": "https://oss2.1dq.com/app/static/app-plus/images/pingtai.png",
+        "text": "全部合同"
+      },
+      {
+        "id": 2,
+        "imgsrc": "https://oss2.1dq.com/app/static/app-plus/images/zaixian.png",
+        "text": "模板"
+      },
+      {
+        "id": 3,
+        "imgsrc": "https://oss2.1dq.com/app/static/app-plus/images/help.png",
+        "text": "帮助中心"
+      },
+      {
+        "id": 4,
+        "imgsrc": "https://oss2.1dq.com/app/static/app-plus/images/zhineng.png",
+        "text": "企业管理"
+      },
+      {
+        "id": 5,
+        "imgsrc": "https://oss2.1dq.com/app/static/app-plus/images/hetongguanli.png",
+        "text": "账户设置"
+      }
+    ],
+    // 轮播图
+    "banner": {
+      "title": "体验无纸化办公",
+      "bannerList": [
+        {
+          "id": 0,
+          "imgsrc": "https://oss2.1dq.com/app/static/app-plus/images/index-b03.jpg"
+        },
+        {
+          "id": 1,
+          "imgsrc": "https://oss2.1dq.com/app/static/app-plus/images/index-b02.jpg"
+        },
+        {
+          "id": 2,
+          "imgsrc": "https://oss2.1dq.com/app/static/app-plus/images/index-b01.jpg"
+        }
+      ]
+    },
+    // 系统模板
+    "model": {
+      "title": "系统模板",
+      "modelList": [
+        {
+          "id": 0,
+          "imgsrc": "../../images/homepage/model1.jpg",
+          "text": "通用合同"
+        },
+        {
+          "id": 1,
+          "imgsrc": "../../images/homepage/model2.jpg",
+          "text": "房屋租赁合同"
+        },
+        {
+          "id": 2,
+          "imgsrc": "../../images/homepage/model3.jpg",
+          "text": "劳动合同"
+        },
+        {
+          "id": 3,
+          "imgsrc": "../../images/homepage/model4.jpg",
+          "text": "安全承诺书"
+        },
+        {
+          "id": 4,
+          "imgsrc": "../../images/homepage/model5.jpg",
+          "text": "公司劳动合同"
+        },
+        {
+          "id": 5,
+          "imgsrc": "../../images/homepage/model6.jpg",
+          "text": "广告合同范本"
+        }
+      ]
     }
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
--- utils/myrequest.js
+++ utils/myrequest.js
@@ -1,17 +1,17 @@
-let myrequest = (options)=>{
+let myrequest = (options) => {
     let url = "http://127.0.0.1:2000" + options.url
     let method = options.method ? options.method : 'GET';
-    return new Promise((resolve,reject)=>{
+    return new Promise((resolve, reject) => {
         wx.request({
-          ...options,
-          url,
-          method,
-          success(res){
-              resolve(res)
-          },
-          fail(error){
-              reject(error)
-          }
+            ...options,
+            url,
+            method,
+            success(res) {
+                resolve(res)
+            },
+            fail(error) {
+                reject(error)
+            }
         })
     })
 }
