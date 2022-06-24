cdb51fbfb19cecaa0448d9058b6c73818e8666a6
EASION\ke <2444579858@qq.com>
2022-06-24 14:44:29 +0800

    看看

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -29,15 +29,12 @@ Page({
     },
     //methods
     loginfn(){
-      wx.navigateTo({
-        url: '登录页面',
-      })
     },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+      
     },
 
     /**
